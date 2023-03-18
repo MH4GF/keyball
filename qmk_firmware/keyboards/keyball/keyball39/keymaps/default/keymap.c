2dabb2697b38a54fc46b933613ffb97933a26e31
MH4GF <h.miyagi.cnw@gmail.com>
2023-03-18 15:31:34 +0900

    fix: Lキーの横に;を配置
    できるだけ通常のキー配置と同じように

--- qmk_firmware/keyboards/keyball/keyball39/keymaps/default/keymap.c
+++ qmk_firmware/keyboards/keyball/keyball39/keymaps/default/keymap.c
@@ -26,7 +26,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   // keymap for default
   [0] = LAYOUT_universal(
     KC_Q     , KC_W     , KC_E     , KC_R     , KC_T     ,                            KC_Y     , KC_U     , KC_I     , KC_O     , KC_P     ,
-    KC_A     , KC_S     , KC_D     , KC_F     , KC_G     ,                            KC_H     , KC_J     , KC_K     , KC_L     , LT(3,KC_SPC),
+    KC_A     , KC_S     , KC_D     , KC_F     , KC_G     ,                            KC_H     , KC_J     , KC_K     , KC_L     , KC_SCLN,
     KC_Z     , KC_X     , KC_C     , KC_V     , KC_B     ,                            KC_N     , KC_M     , KC_COMM  , KC_DOT   , KC_BSPC  ,
     KC_LSFT  , _______ , _______ , _______, LGUI_T(KC_LNG2), LT(1,KC_SPC),   KC_ENT, LT(2,KC_BSPC), LT(3,KC_LNG1), KC_RGUI, KC_RSFT, KC_SLSH
   ),
